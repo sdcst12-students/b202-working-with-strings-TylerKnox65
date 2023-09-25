21d6d3c8d6ef65ed0f31be5021c28e9b63a70437
TylerKnox65 <tylerj652@deltalearns.ca>
2023-09-25 12:16:45 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -54,8 +54,23 @@ for i in list(html):
         begin = html.find("{")
         end = html.find("}")
         html = html[0:begin] + html[end+1:len(html)]
-
+print
+html = [x for x in html]
+html = list(html)
+print(len(html))
 print(html)
+n = 0
+'''
+for i in html:
+    #print()
+    if i == " ":
+        html.pop(html.index(i))
+    if i == "." or i == ";" or i == "!":
+        html.insert(html.index(i), "\n")
+    n+=1
+html = " ".join(html)
+print(html)
+'''
     
     
 
